1c69f57fd74f750cb54eade8e827bc63f46fb5f8
abdo12249 <abdobwd87@gmail.com>
2025-01-18 15:17:44 +0200

    1111

--- test/mobile.js
+++ test/mobile.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // تحديد الزر والقائمة
-    const menuBtn = document.getElementById('menu-btn');
-    const sidebar = document.getElementById('sidebar');
+// تحديد الزر والقائمة
+const menuBtn = document.getElementById('menu-btn');
+const sidebar = document.getElementById('sidebar');
 
-    // تحقق من وجود العناصر قبل إضافة الحدث
-    if (menuBtn && sidebar) {
-        // عند الضغط على الزر
-        menuBtn.addEventListener('click', () => {
-            sidebar.classList.toggle('active'); // فتح أو غلق القائمة
-        });
-    }
+// عند الضغط على الزر
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active'); // فتح أو غلق القائمة
 });
