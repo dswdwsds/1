4434437dfc8c2ef5a6d818295c18e97a75a79b6c
abdo12249 <abdobwd87@gmail.com>
2025-01-18 11:49:21 +0200

    111

--- navbar.js
+++ navbar.js
@@ -22,13 +22,12 @@
         // يمكنك هنا تنفيذ أي عملية بحث بناءً على النص المدخل
         console.log('بحث عن: ', query);
     });
-    // تحديد الزر والقائمة
+// تحديد الزر والقائمة
 const menuBtn = document.getElementById('menu-btn');
 const sidebar = document.getElementById('sidebar');
 
 // عند الضغط على الزر
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active'); // إضافة/إزالة تأثير الأنيميشن للزر
     sidebar.classList.toggle('active'); // فتح أو غلق القائمة
 });
 
