7698463d3c43aa9c2058bf4233d56ca6a6293fd5
abdo12249 <abdobwd87@gmail.com>
2025-02-04 14:16:34 +0200

    11

--- navbar.js
+++ navbar.js
@@ -1,42 +1,40 @@
 // وظيفة للتحقق من وجود العناصر وإعادة المحاولة إذا لزم الأمر
 function initializeElements() {
-// عنصر تبديل الثيم
-// عنصر تبديل الثيم
-const themeToggle = document.getElementById('themeToggle');
-
-// دالة لتبديل الثيم وتخزين الحالة
-function toggleTheme() {
-    if (document.body.classList.contains('dark-mode')) {
-        document.body.classList.add('light-mode');
-        document.body.classList.remove('dark-mode');
-        localStorage.setItem('theme', 'light'); // حفظ الثيم في الذاكرة
-    } else {
-        document.body.classList.add('dark-mode');
-        document.body.classList.remove('light-mode');
-        localStorage.setItem('theme', 'dark'); // حفظ الثيم في الذاكرة
-    }
-}
-
-// التحقق من وجود الثيم المحفوظ عند تحميل الصفحة
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-        document.body.classList.remove('light-mode');
-    } else {
-        document.body.classList.add('light-mode');
-        document.body.classList.remove('dark-mode');
-    }
-}
-
-// إضافة الحدث بعد التأكد من تحميل الصفحة بالكامل
-window.addEventListener('load', function() {
-    if (themeToggle) {
-        themeToggle.addEventListener('click', toggleTheme);
-    } else {
-        console.log("لم يتم العثور على themeToggle.");
-    }
-});
+    document.addEventListener('DOMContentLoaded', function() {
+        const themeToggle = document.getElementById('themeToggle');
+        
+        // دالة لتبديل الثيم وتخزين الحالة
+        function toggleTheme() {
+            if (document.body.classList.contains('dark-mode')) {
+                document.body.classList.add('light-mode');
+                document.body.classList.remove('dark-mode');
+                localStorage.setItem('theme', 'light'); // حفظ الثيم في الذاكرة
+            } else {
+                document.body.classList.add('dark-mode');
+                document.body.classList.remove('light-mode');
+                localStorage.setItem('theme', 'dark'); // حفظ الثيم في الذاكرة
+            }
+        }
+    
+        // التحقق من وجود الثيم المحفوظ عند تحميل الصفحة
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            if (savedTheme === 'dark') {
+                document.body.classList.add('dark-mode');
+                document.body.classList.remove('light-mode');
+            } else {
+                document.body.classList.add('light-mode');
+                document.body.classList.remove('dark-mode');
+            }
+        }
+    
+        if (themeToggle) {
+            themeToggle.addEventListener('click', toggleTheme);
+        } else {
+            console.log("لم يتم العثور على themeToggle.");
+        }
+    });
+    
 
 
     // عنصر البحث
