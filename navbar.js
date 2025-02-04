31b70cf3a68a7ff088f1b0908721cfeff27a94bd
abdo12249 <abdobwd87@gmail.com>
2025-02-04 14:35:40 +0200

    11

--- navbar.js
+++ navbar.js
@@ -23,7 +23,7 @@ if (themeToggle) {
  // تحميل البيانات من ملف JSON
 let articles = [];
 
-fetch('قائمة الأنمي/قائمة الأنمي.json')
+fetch('navbar/قائمة الأنمي/قائمة الأنمي.json')
   .then(response => response.json())
   .then(data => {
     articles = data;  // حفظ البيانات في متغير
